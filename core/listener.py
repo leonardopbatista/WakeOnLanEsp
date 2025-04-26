0780a6dc4a3bcfa299212a86e247d9b3d358af14
Leonardo Pires Batista <leonardopbatista98@gmail.com>
2025-04-25 23:54:25 -0300

    Refactoring

--- core/listener.py
+++ core/listener.py
@@ -1,3 +1,4 @@
+import contextlib
 from threading import Thread, Event
 from queue import Queue
 import time
@@ -11,16 +12,15 @@ def check_connection(brokers: list[str], topic_feedback: str, timeout: int = 20)
     def listener(broker: str):
         def on_message(client: Client, userdata, msg: MQTTMessage):
             payload = msg.payload.decode().strip()
-            if payload == "PING:ONLINE":
-                if not done_event.is_set():
-                    result_queue.put(broker)
-                    done_event.set()
-                    client.disconnect()
+            if payload == "PING:ONLINE" and not done_event.is_set():
+                result_queue.put(broker)
+                done_event.set()
+                client.disconnect()
 
         client = Client()
         client.on_message = on_message
 
-        try:
+        with contextlib.suppress(Exception):
             client.connect(broker, 1883, 60)
             client.subscribe(topic_feedback)
             client.loop_start()
@@ -31,13 +31,11 @@ def check_connection(brokers: list[str], topic_feedback: str, timeout: int = 20)
 
             client.loop_stop()
             client.disconnect()
-        except:
-            pass
 
     for broker in brokers:
         Thread(target=listener, args=(broker,), daemon=True).start()
     try:
         broker_found = result_queue.get(timeout=timeout)
         return broker_found
-    except:
+    except Exception:
         return None
--- core/sender.py
+++ core/sender.py
@@ -1,3 +1,4 @@
+import contextlib
 import time
 import paho.mqtt.client as mqtt
 
@@ -22,26 +23,24 @@ def send_wol_command(broker: str, topic_command: str, topic_feedback: str) -> bo
             return True
         except Exception as e:
             return False
-    
+
     client = mqtt.Client(userdata={'broker': broker})
     client.on_message = on_message
-    
+
     if not try_broker_connection(client, broker, topic_feedback):
         return False
     try:
         client.publish(topic_command, "ON", qos=1)
 
         timeout = time.time() + 12
-        while time.time() < timeout and feedback == False:
+        while time.time() < timeout and not feedback:
             time.sleep(0.1)
-        
+
         return feedback
-    
+
     except Exception as e:
         return False
     finally:
         client.loop_stop()
-        try:
+        with contextlib.suppress(Exception):
             client.disconnect()
-        except:
-            pass
